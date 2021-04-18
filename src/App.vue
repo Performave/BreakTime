12b7e685827837e98f9aea9b466ad19977f4e117
Eric Wang <msexploiting@gmail.com>
2021-04-18 15:02:52 -0500

    Made more visually appealing

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+html, body {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to right, #2e3048, #2f314a, #30324c, #32344e, #333550, #333550, #333550, #333550, #32344e, #30324c, #2f314a, #2e3048);
+}
+
 #app {
+  width: 100%;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #fff;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="flex justify-center mt-52">
-    <div class="w-36 h-36 outline-black">
+  <div class="flex items-center justify-center w-full h-full absolute">
+    <div class="bg-container">
       <div
         class="absolute box"
         v-for="index in 36"
         :style="{
-          transform: 'translateY(-5px) rotate(calc(' + index + ' * 10deg))',
+          transform:
+            'translateY(-5px) rotate(calc(' + index + ' * 10deg))',
         }"
         :key="index"
       >
-        <span class="rounded-full" :style="{filter: 'hue-rotate('+index+'*5deg)'}"></span>
+        <span
+          class="rounded-full"
+          :style="{ filter: 'hue-rotate(calc(' + index + ' *.5deg))' }"
+        ></span>
       </div>
     </div>
+  </div>
+
+  <div class="grid place-items-center w-full h-full">
+    <h1 class="text-6xl font-bold">4:20</h1>
   </div>
 </template>
 
@@ -24,14 +32,20 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.bg-container {
+  width: 400px;
+  height: 400px;
+}
+
 .box {
   width: 400px;
   height: 6px;
   position: absolute;
+  top: 50%;
 }
 
 .box span {
-  background: #E55A70;
+  background: #e55a70;
   display: block;
   width: 80px;
   height: 6px;
