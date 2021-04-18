689228b1816048d5949c39014dc9bf41f5a270e3
Eric Wang <msexploiting@gmail.com>
2021-04-18 14:41:55 -0500

    Added ring circle

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
-  <h1 class="text-5xl">test</h1>
-
-  <div class="w-52 h-52 border-dotted border-8 border-light-blue-500 rounded-full">
+  <div class="flex justify-center mt-52">
+    <div class="w-36 h-36 outline-black">
+      <div
+        class="absolute box"
+        v-for="index in 36"
+        :style="{
+          transform: 'translateY(-5px) rotate(calc(' + index + ' * 10deg))',
+        }"
+        :key="index"
+      >
+        <span class="rounded-full" :style="{filter: 'hue-rotate('+index+'*5deg)'}"></span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,3 +22,18 @@ export default defineComponent({
   name: "Home",
 });
 </script>
+
+<style scoped>
+.box {
+  width: 400px;
+  height: 6px;
+  position: absolute;
+}
+
+.box span {
+  background: #E55A70;
+  display: block;
+  width: 80px;
+  height: 6px;
+}
+</style>
