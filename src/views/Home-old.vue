291b88b42737428bd3a718352533eb379eb092f0
Eric W <37554696+SynEnt@users.noreply.github.com>
2021-04-18 09:22:11 -0500

    Added border circle

---
+++ src/views/Home-old.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1 class="text-5xl">test</h1>
+  <h1 class="text-5xl font-bold text-center">Increment me</h1>
+  <h3 class="text-center">{{ counter }}</h3>
+  <div class="flex justify-center">
+    <button class="bg-red-500 py-2 px-5 rounded-md" @click="counter++">Increment</button>
+  </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    const store = useStore();
+
+    
+    const counter = computed({
+      get: () => store.state.count,
+      set: (value: number) => store.commit('increment', value),
+    });
+
+    return {
+      counter
+    };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,14 @@
 <template>
-    <h1 class="text-5xl">test</h1>
-  <h1 class="text-5xl font-bold text-center">Increment me</h1>
-  <h3 class="text-center">{{ counter }}</h3>
-  <div class="flex justify-center">
-    <button class="bg-red-500 py-2 px-5 rounded-md" @click="counter++">Increment</button>
-  </div>
+  <h1 class="text-5xl">test</h1>
 
+  <div class="w-52 h-52 border-dotted border-8 border-light-blue-500 rounded-full">
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
-import { useStore } from 'vuex';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
-  setup() {
-    const store = useStore();
-
-    
-    const counter = computed({
-      get: () => store.state.count,
-      set: (value: number) => store.commit('increment', value),
-    });
-
-    return {
-      counter
-    };
-  },
+  name: "Home",
 });
 </script>
