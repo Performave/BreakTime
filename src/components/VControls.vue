3c4689dce0d3ebe376ed664be707c5067fd78337
Eric Wang <msexploiting@gmail.com>
2021-04-18 22:05:45 -0500

    Added cancel option to VControls

--- src/components/VControls.vue
+++ src/components/VControls.vue
@@ -9,12 +9,35 @@
       </button>
     </template>
     <template v-if="active">
-      <button
-        class="py-4 px-10 font-bold rounded-full transition-all bg-gray-100 text-pink-500 focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-900 focus:ring-red-500 text-xl uppercase active:outline-none focus:outline-none outline-none"
-        @click="toggleTimer"
-      >
-        Pause
-      </button>
+      <div class="flex items-center justify-center w-full">
+        <div class="flex justify-end w-1/3 px-8">
+          <button class="p-3 h-12 w-12 button-muted rounded-full">
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon-muted"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+          </button>
+        </div>
+        <div class="flex justify-center w-1/3">
+          <button
+            class="py-4 px-10 font-bold rounded-full transition-all bg-gray-100 text-pink-500 focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-900 focus:ring-red-500 text-xl uppercase active:outline-none focus:outline-none outline-none"
+            @click="toggleTimer"
+          >
+            Pause
+          </button>
+        </div>
+        <div class="w-1/3"></div>
+      </div>
     </template>
   </div>
 </template>
@@ -28,8 +51,8 @@ export default defineComponent({
     const active = ref(false);
 
     const toggleTimer = () => {
-      active.value = !active.value
-    }
+      active.value = !active.value;
+    };
 
     return {
       active,
@@ -40,4 +63,11 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.button-muted {
+  background-color: #3B3D5A;
+}
+
+.icon-muted {
+  color: #8487A8;
+}
 </style>
